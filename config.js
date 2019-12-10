40ad22e007a7bdf6fcf984ea3a4690a030a18eb6
Virink <virink@outlook.com>
2019-12-10 13:13:22 +0800

    Update: config.js

--- config.js
+++ config.js
@@ -7,3 +7,7 @@ module.exports = {
         name: 'article_crawler'
     }
 }
+
+// create database article_crawler;
+// grant all privileges on article_crawler.* to crawler@'127.0.0.1' identified by '123456';
+// flush privileges;
